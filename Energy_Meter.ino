fc84d7a75e27e774dcdf1ec9302821e9d9245e8a
Saalim <67445603+s-a-usman@users.noreply.github.com>
2023-12-20 20:20:41 +0100

    Update Energy_Meter.ino

--- Energy_Meter.ino
+++ Energy_Meter.ino
@@ -1,7 +1,3 @@
-kirkusreview.com/about/careers
-reedsy.com/discovery
-booklistonline.com
-onlinebookclub.org
 /**********************************************************************************
  *  Preferences--> Aditional boards Manager URLs : 
  *  https://dl.espressif.com/dl/package_esp32_index.json, http://arduino.esp8266.com/stable/package_esp8266com_index.json
